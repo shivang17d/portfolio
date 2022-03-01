0868822d0657099336f67a03c0ea0143293ba120
Shivang Gupta <shivangguptaxd@gmail.com>
2022-03-01 12:30:59 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,7 +3,7 @@
 // Website related settings
 const settings = {
   isSplash: true, // Change this to true if you want to use the splash screen.
-  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  useCustomCursor: false, // Change this to false if you want the good'ol cursor
   googleTrackingID: "UA-174238252-2",
 };
 
